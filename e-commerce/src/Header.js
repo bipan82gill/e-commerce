2c4200d9fcad2df328e23e52c04010242884b6f8
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-09-09 20:13:49 -0400

    done code with nav bar

--- e-commerce/src/Header.js
+++ e-commerce/src/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
-
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 function Header() {
     return (
         <div className="header">
@@ -18,25 +18,29 @@ function Header() {
                         <span className="header__optionLineOne">
                            hello
                         </span>
-                        <span className="header__optionTwoOne">
+                        <span className="header__optionLineTwo">
                            Sign In
                         </span>
                     </div>
-                    <div className ="'header__option">
+                    <div className ="header__option">
                         <span className="header__optionLineOne">
                            returns
                         </span>
-                        <span className="header__optionTwoOne">
+                        <span className="header__optionLineTwo">
                            & Orders
                         </span>
                     </div>
-                    <div className ="'header__option">
+                    <div className ="header__option">
                         <span className="header__optionLineOne">
                            Your
                         </span>
-                        <span className="header__optionTwoOne">
+                        <span className="header__optionLineTwo">
                            Prime
                         </span>
+                    </div>
+                    <div className ="header__optionBasket">
+                        <ShoppingBasketIcon/>
+                        <span className="header__optionLineTwo header__basketCount">0</span>
                     </div>
                 </div>
         </div>
