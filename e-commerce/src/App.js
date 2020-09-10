b5210d69a0c49ca72d2e2da76ec36f5e3475e342
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-09-10 18:52:04 -0400

    setup routes

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 // import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Header/>
-      <Home/>
+      <Switch>
+        <Route path ='/checkout'>
+          <Header/>
+          <h1>I AM A CHECKOUT , SMASH THE LIKE BUTTON</h1>
+        </Route>
+        <Route path ='/'>
+          <Header/>
+          <Home/>
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
