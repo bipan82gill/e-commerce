ed147380422a5629a1bd8bea3c98d02f600672d2
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-09-09 22:22:57 -0400

    creating home page

---
+++ e-commerce/src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css';
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__container">
+                <img  className="home__image" src="https://t4.ftcdn.net/jpg/03/08/35/27/240_F_308352773_HhRgXeuYclHCKwsxcP9D82MDv98jKEWR.jpg" alt="product"/>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home
