842500b7ab8fab96625d614a9c0cfddbbe934c80
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-09-12 16:58:07 -0400

    rendering remove from basket

--- e-commerce/src/CheckoutProduct.js
+++ e-commerce/src/CheckoutProduct.js
@@ -2,8 +2,17 @@ import React from 'react';
 import './CheckoutProduct.css';
 import { getBasketTotal } from './reducer';
 import StarIcon from '@material-ui/icons/Star';
+import { useStateValue } from './StateProvider';
 
 function CheckoutProduct({id, title, image, price, rating}) {
+   const [{basket}, dispatch] = useStateValue();
+
+   const removeFromBasket = () => {
+       dispatch({
+           type:"REMOVE_FROM_BASKET",
+           id: id
+       })
+   }
     return (
         <div className ="checkoutProduct">
             <img className ="checkoutProduct__image" src={image}/>
@@ -20,7 +29,7 @@ function CheckoutProduct({id, title, image, price, rating}) {
                         <StarIcon/>
                     ))}
                 </div>
-                <button>Remove from Basket</button>
+                <button onClick={removeFromBasket}>Remove from Basket</button>
             </div>
         </div>
     )
