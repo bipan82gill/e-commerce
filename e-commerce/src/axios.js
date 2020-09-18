0334eef41add47b970305cfdccc040257a039708
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-09-18 19:53:24 -0400

    deploy app

--- e-commerce/src/axios.js
+++ e-commerce/src/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/e-commerce-5fa7c/us-central1/api'
+    baseURL: 'https://us-central1-e-commerce-5fa7c.cloudfunctions.net/api'
+    // 'http://localhost:5001/e-commerce-5fa7c/us-central1/api'
 });
 export default instance;
